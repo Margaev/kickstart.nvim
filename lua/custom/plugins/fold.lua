b3d43673321c05ff3d88fa014cbef9f6a98f3e5e
Margaev <andrei.margaev@gmail.com>
2025-09-16 00:20:44 +0200

    default fold

--- lua/custom/plugins/fold.lua
+++ lua/custom/plugins/fold.lua
@@ -6,7 +6,7 @@ return {
     },
     config = function()
       vim.o.foldcolumn = '1'
-      vim.o.foldlevel = 99
+      vim.o.foldlevel = 0
       vim.o.foldlevelstart = 99
       vim.o.foldenable = true
 
