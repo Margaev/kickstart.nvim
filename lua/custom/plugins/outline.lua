9a5c96523b4d3e37c215eb89f5fbc6994f3a35c1
Margaev <andrei.margaev@gmail.com>
2025-10-24 17:22:46 +0200

    changes

--- lua/custom/plugins/outline.lua
+++ lua/custom/plugins/outline.lua
@@ -9,11 +9,9 @@ return {
     },
     config = function()
       require('aerial').setup {
-        -- optionally use on_attach to set keymaps when aerial has attached to a buffer
         on_attach = function(bufnr)
-          -- Jump forwards/backwards with '{' and '}'
-          vim.keymap.set({ 'n', 'v' }, '[[', '<cmd>AerialPrev<CR>', { buffer = bufnr })
-          vim.keymap.set({ 'n', 'v' }, ']]', '<cmd>AerialNext<CR>', { buffer = bufnr })
+          vim.keymap.set({ 'n', 'v' }, '<leader>[', '<cmd>AerialPrev<CR>', { buffer = bufnr })
+          vim.keymap.set({ 'n', 'v' }, '<leader>]', '<cmd>AerialNext<CR>', { buffer = bufnr })
         end,
         autojump = true,
       }
