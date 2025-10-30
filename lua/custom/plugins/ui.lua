56b3338d34edba463086f205c61928a5fdf6e3b9
Margaev <andrei.margaev@gmail.com>
2025-10-30 14:22:12 +0100

    update

---
+++ lua/custom/plugins/ui.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- require('notify').setup {
+      --   merge_duplicates = true,
+      --   top_down = false,
+      -- },
+    },
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+    },
+  },
+}
