25932b149cb8bc0b97c161c8e999e0685a0dba58
Margaev <andrei.margaev@gmail.com>
2025-11-02 23:42:44 +0100

    updates

---
+++ lua/custom/plugins/strudel.lua
@@ -0,0 +1,11 @@
+-- vim.keymap.set('n', '<leader>gb', '<Cmd>StrudelLaunch<CR>', { desc = '[g]it [b]lame' })
+
+return {
+  -- {
+  --   'gruvw/strudel.nvim',
+  --   build = 'npm install',
+  --   config = function()
+  --     require('strudel').setup()
+  --   end,
+  -- },
+}
