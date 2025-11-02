31212f2792c5edb9988a2473c03a69c96dc5bf25
Margaev <andrei.margaev@gmail.com>
2025-11-02 11:45:43 +0100

    updates

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -42,6 +42,15 @@ vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufWritePost' }, {
   end,
 })
 
+-- TODO: check if it fixed tab jumping
+vim.api.nvim_create_autocmd('InsertLeave', {
+  callback = function()
+    if require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()] and not require('luasnip').session.jump_active then
+      require('luasnip').unlink_current()
+    end
+  end,
+})
+
 return {
   {
     'akinsho/toggleterm.nvim',
