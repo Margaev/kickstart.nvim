fd4e87a33ce31c8f1ea494cb5fcfecd4624455a4
Margaev <andrei.margaev@gmail.com>
2025-11-06 23:38:29 +0100

    updates

--- lua/custom/plugins/dap.lua
+++ lua/custom/plugins/dap.lua
@@ -39,11 +39,12 @@ return {
       end)
 
       vim.keymap.set('n', '<leader>dc', dap.continue, { desc = 'continue' })
-      vim.keymap.set('n', '<leader>de', dap.terminate, { desc = 'terminate' })
-      vim.keymap.set('n', '<leader>di', dap.step_into, { desc = 'step into' })
-      vim.keymap.set('n', '<leader>do', dap.step_over, { desc = 'step over' })
-      vim.keymap.set('n', '<leader>du', dap.step_out, { desc = 'step out' })
-      vim.keymap.set('n', '<leader>dsb', dap.step_back, { desc = 'step back' })
+      vim.keymap.set('n', '<leader>dl', dap.step_into, { desc = 'step into' })
+      vim.keymap.set('n', '<leader>dj', dap.step_over, { desc = 'step over' })
+      vim.keymap.set('n', '<leader>dh', dap.step_out, { desc = 'step out' })
+      vim.keymap.set('n', '<leader>du', dap.run_to_cursor, { desc = 'run to cursor' })
+      -- vim.keymap.set('n', '<leader>dsb', dap.step_back, { desc = 'step back' })
+      vim.keymap.set('n', '<leader>dt', dap.terminate, { desc = 'terminate' })
       vim.keymap.set('n', '<leader>dr', dap.restart, { desc = 'restart' })
 
       dap.listeners.before.attach.dapui_config = function()
