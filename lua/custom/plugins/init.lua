45bf148b5b894c132f236cccdaf2eebf0bac44ee
Margaev <andrei.margaev@gmail.com>
2025-11-13 10:13:06 +0100

    changes

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -267,7 +267,9 @@ return {
     'Wansmer/treesj',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     config = function()
-      require('treesj').setup {}
+      require('treesj').setup {
+        use_default_keymaps = false,
+      }
       vim.keymap.set('n', '<leader>tj', '<Cmd>TSJToggle<CR>')
     end,
   },
