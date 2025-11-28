657a16e8a58484ce9c84615d2aeb42dd8ba51618
Margaev <andrei.margaev@gmail.com>
2025-11-28 17:08:42 +0100

    changes

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -20,6 +20,17 @@ return {
           enabled = true,
         },
       }
+      require('lualine').setup {
+        sections = {
+          lualine_x = {
+            {
+              require('noice').api.statusline.mode.get,
+              cond = require('noice').api.statusline.mode.has,
+              color = { fg = '#ff9e64' },
+            },
+          },
+        },
+      }
     end,
     dependencies = {
       'MunifTanjim/nui.nvim',
