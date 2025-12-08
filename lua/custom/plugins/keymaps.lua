eedd35da89f6107397772e44110c4f77a234bc6f
Margaev <andrei.margaev@gmail.com>
2025-12-08 09:26:38 +0100

    updates

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -58,4 +58,10 @@ vim.keymap.set('n', '<leader>N', function()
   require('nvim-navbuddy').open()
 end, { desc = 'Open MarkdownPreview' })
 
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+vim.keymap.set('x', '<leader>p', [["_dP]])
+vim.keymap.set({ 'n', 'v' }, '<leader>d', '"_d')
+
 return {}
